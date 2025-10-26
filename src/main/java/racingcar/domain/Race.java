61a0b342ee6d358d8678b0993a43ea14667950f0
partmant <ghdckdgml191@gmail.com>
2025-10-26 23:34:41 +0900

    feat(Race): 리팩토링 및 책임 분리
    - Cars 객체의 상태 관리 책임과 Race 객체의 행위 실행 책임 분리
    - moveAllCars() 메서드가List<Car>를 인자로 받도록 수정

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -4,21 +4,15 @@ import java.util.List;
 
 public class Race {
 
-    private final List<Car> cars;
     private final NumberGenerator generator;
 
-    public Race(List<Car> cars, NumberGenerator generator) {
-        this.cars = cars;
+    public Race(NumberGenerator generator) {
         this.generator = generator;
     }
 
-    public void moveAllCars() {
+    public void moveAllCars(List<Car> cars) {
         for (Car car : cars) {
             car.move(generator);
         }
-    }
-
-    public List<Car> getCars() {
-        return cars;
     }
 }
--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -9,14 +9,31 @@ import org.junit.jupiter.api.Test;
 
 class RaceTest {
 
+    private static class TestCar extends Car {
+        public TestCar(String name) {
+            super(name);
+        }
+    }
+
+    private static class FixedNumberGenerator implements NumberGenerator {
+        private final int number;
+        public FixedNumberGenerator(int number) {
+            this.number = number;
+        }
+        @Override
+        public int generate() {
+            return number;
+        }
+    }
+
     @Test
     @DisplayName("모든 자동차가 이동 조건을 만족하면 모두 전진한다")
     void allCarsMoveWhenNumberIsAtLeastThreshold() {
-        List<Car> cars = Arrays.asList(new Car("pobi"), new Car("woni"));
-        NumberGenerator generator = () -> 4;
-        Race race = new Race(cars, generator);
+        List<Car> cars = Arrays.asList(new TestCar("pobi"), new TestCar("woni"));
+        NumberGenerator generator = new FixedNumberGenerator(4);
+        Race race = new Race(generator);
 
-        race.moveAllCars();
+        race.moveAllCars(cars);
 
         assertThat(cars)
                 .extracting(Car::getPosition)
@@ -26,25 +43,14 @@ class RaceTest {
     @Test
     @DisplayName("모든 자동차가 이동 조건을 만족하지 않으면 모두 정지한다")
     void allCarsDoNotMoveWhenNumberIsBelowThreshold() {
-        List<Car> cars = Arrays.asList(new Car("pobi"), new Car("woni"), new Car("jun"));
-        NumberGenerator generator = () -> 3;
-        Race race = new Race(cars, generator);
+        List<Car> cars = Arrays.asList(new TestCar("pobi"), new TestCar("woni"), new TestCar("jun"));
+        NumberGenerator generator = new FixedNumberGenerator(3);
+        Race race = new Race(generator);
 
-        race.moveAllCars();
+        race.moveAllCars(cars);
 
         assertThat(cars)
                 .extracting(Car::getPosition)
                 .containsExactly(0, 0, 0);
-    }
-
-    @Test
-    @DisplayName("자동차 목록을 반환한다")
-    void returnsCarList() {
-        List<Car> cars = Arrays.asList(new Car("pobi"), new Car("woni"));
-        Race race = new Race(cars, () -> 4);
-
-        List<Car> result = race.getCars();
-
-        assertThat(result).containsExactlyElementsOf(cars);
     }
 }
