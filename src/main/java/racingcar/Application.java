b271fe510f2516ac9576f0109a0446664d9d9a6d
partmant <ghdckdgml191@gmail.com>
2025-10-27 00:39:50 +0900

    feat(controller): 경주 흐름을 제어하는 RacingController 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.domain.Race;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
+        Race race = new Race(numberGenerator);
+        RacingService racingService = new RacingService(race);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        RacingController controller = new RacingController(inputView, outputView, racingService);
+        controller.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,65 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.service.RacingService;
+import racingcar.util.AttemptCountValidator;
+import racingcar.util.CarNameParser;
+import racingcar.util.CarNameValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+
+    public RacingController(InputView inputView, OutputView outputView, RacingService racingService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+    }
+
+    public void run() {
+        Cars cars = initializeCars();
+        int attemptCount = readAttemptCount();
+
+        executeRaceRounds(cars, attemptCount);
+        displayFinalResults(cars);
+    }
+
+    private Cars initializeCars() {
+        String inputNames = inputView.readCarNames();
+        List<String> carNames = CarNameParser.splitCarName(inputNames);
+        CarNameValidator.validate(carNames);
+
+        List<Car> carList = carNames.stream()
+                .map(Car::new)
+                .toList();
+
+        return new Cars(carList);
+    }
+
+    private int readAttemptCount() {
+        String inputAttempt = inputView.readAttemptCount();
+        return AttemptCountValidator.validate(inputAttempt);
+    }
+
+    private void executeRaceRounds(Cars cars, int attemptCount) {
+        outputView.printResultMessage();
+
+        for (int round = 0; round < attemptCount; round++) {
+            racingService.startRace(cars, 1);
+            outputView.printRoundResult(cars.getCarNames(), cars.getPositions());
+        }
+    }
+
+    private void displayFinalResults(Cars cars) {
+        List<String> winners = cars.getWinners();
+        outputView.printWinners(winners);
+        System.out.print(outputView.getOutputResult());
+    }
+}
