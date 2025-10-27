c427b3ba9932574e327510572ce613594053c01e
partmant <ghdckdgml191@gmail.com>
2025-10-27 21:02:36 +0900

    refactor(controller): 사용되지 않는 outputHandler 필드 제거

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,19 +1,17 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
-import racingcar.service.RacingService;
 import racingcar.io.InputHandler;
 import racingcar.io.OutputHandler;
+import racingcar.service.RacingService;
 
 public class RacingController {
 
     private final InputHandler inputHandler;
-    private final OutputHandler outputHandler;
     private final RaceManager raceManager;
 
-    private RacingController(InputHandler inputHandler, OutputHandler outputHandler, RaceManager raceManager) {
+    private RacingController(InputHandler inputHandler, RaceManager raceManager) {
         this.inputHandler = inputHandler;
-        this.outputHandler = outputHandler;
         this.raceManager = raceManager;
     }
 
@@ -21,7 +19,7 @@ public class RacingController {
         InputHandler inputHandler = new InputHandler();
         OutputHandler outputHandler = new OutputHandler();
         RaceManager raceManager = new RaceManager(racingService, outputHandler);
-        return new RacingController(inputHandler, outputHandler, raceManager);
+        return new RacingController(inputHandler, raceManager);
     }
 
     public void run() {
