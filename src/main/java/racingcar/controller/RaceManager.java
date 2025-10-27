bf45921f94e8f6d74fe22cbb002a4ebd9e1c258f
partmant <ghdckdgml191@gmail.com>
2025-10-27 20:59:55 +0900

    refactor(RaceManager):  메서드를 세분화하여 단일 책임 명확화

--- src/main/java/racingcar/controller/RaceManager.java
+++ src/main/java/racingcar/controller/RaceManager.java
@@ -1,8 +1,8 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
-import racingcar.service.RacingService;
 import racingcar.io.OutputHandler;
+import racingcar.service.RacingService;
 
 public class RaceManager {
 
@@ -15,13 +15,27 @@ public class RaceManager {
     }
 
     public void runRace(Cars cars, int attemptCount) {
+        printHeader();
+        executeRounds(cars, attemptCount);
+        printFinal(cars);
+    }
+
+    private void printHeader() {
         outputHandler.printResultHeader();
+    }
 
+    private void executeRounds(Cars cars, int attemptCount) {
         for (int round = 0; round < attemptCount; round++) {
-            racingService.startRound(cars);
-            outputHandler.printRoundResult(cars);
+            playSingleRound(cars);
         }
+    }
 
+    private void playSingleRound(Cars cars) {
+        racingService.startRound(cars);
+        outputHandler.printRoundResult(cars);
+    }
+
+    private void printFinal(Cars cars) {
         outputHandler.printFinalWinners(cars);
     }
 }
