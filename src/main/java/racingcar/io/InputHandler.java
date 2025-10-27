769cb14a81461ffa186fa8ca685b23c10c79ea3b
partmant <ghdckdgml191@gmail.com>
2025-10-27 20:57:07 +0900

    refactor(InputHandler):  메서드를 세분화하여 단일 책임 명확화

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -11,14 +11,38 @@ import java.util.List;
 public class InputHandler {
 
     public Cars getCarsFromUser() {
-        String inputNames = InputView.readCarNames();
-        List<String> carNames = CarNameParser.splitCarName(inputNames);
-        CarNameValidator.validate(carNames);
-        return Cars.create(carNames);
+        String inputNames = readCarNames();
+        List<String> carNames = parseCarNames(inputNames);
+        validateCarNames(carNames);
+        return createCars(carNames);
     }
 
     public int getAttemptCountFromUser() {
-        String inputAttempt = InputView.readAttemptCount();
+        String inputAttempt = readAttemptCount();
+        return validateAttemptCount(inputAttempt);
+    }
+
+    private String readCarNames() {
+        return InputView.readCarNames();
+    }
+
+    private List<String> parseCarNames(String inputNames) {
+        return CarNameParser.splitCarName(inputNames);
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        CarNameValidator.validate(carNames);
+    }
+
+    private Cars createCars(List<String> carNames) {
+        return Cars.create(carNames);
+    }
+
+    private String readAttemptCount() {
+        return InputView.readAttemptCount();
+    }
+
+    private int validateAttemptCount(String inputAttempt) {
         return AttemptCountValidator.validate(inputAttempt);
     }
 }
